4aa95e84fd78bf25d5415addfe208a59df814462
aureliewouy <aureliecedia@yahoo.com>
2023-09-01 18:25:28 +0200

    animation + media

--- src/components/bioSection.js
+++ src/components/bioSection.js
@@ -1,17 +1,21 @@
 import AureWeb from "../medias/aureliebeige.png";
 import { connect } from 'react-redux';
 
-import '../css/biosection.css';
 import '../App.css';
+import '../css/biosection.css';
 const BioSection = ({ isNightMode }) => {
 
     return (
         <div className={`biosection ${isNightMode ? 'night-mode-no-bg' : 'day-mode-no-bg'}`}>
-            <div className="slide-in">
-            <img  className="biophoto selectDisable" src={AureWeb} alt="bio aurelie">
-            </img>
+            <div className="slide-in photocontainer" >
+            <img  className="biophoto selectDisable" src={AureWeb} alt="bio aurelie"/>
+            <div className="photohide"></div>
+            <div className="photohide"></div>
+            <div className="photohide"></div>
+            {/* <div className="photohide"></div> */}
+            {/* <div className="photohide"></div> */}
             </div>
-            <p  style={{textAlign: 'left'}} className="activeLeft">   Hey, je suis Aurélie j'ai 28 ans et j'habite à Paris.<br/>Je relève actuellement un défi. Mon expérience en startup a duré plus d'un an, en fullstack et devops. Imagine, je touchais à tout, même à la machine à café. Mais, tiens-toi bien, une révélation : j'ai un faible pour le front-end. Alors devine où je m'oriente maintenant ? Tout droit vers ce terrain captivant ! 😉</p>
+            <p  style={{textAlign: 'left'}} className="activeLeft">   Hey, je suis Aurélie j'ai 28 ans et j'habite à Paris.<br/>Mon expérience en startup a duré plus d'un an, en fullstack et devops. Imaginez, je touchais à tout, même à la machine à café. Mais, révélation  ! J'ai un faible pour le front-end. Alors devine où je m'oriente maintenant ? Tout droit vers ce terrain captivant ! 😉</p>
         </div>
     );
 }
